ad588cc4884781033bea912535fa8068c2165f0f
Guillaume SAINTHILLIER <guillaume.sainthillier@gmail.com>
2014-09-19 20:14:22 +0200

    Changement de l'URL de l'API

--- web/app.php
+++ web/app.php
@@ -1,6 +1,6 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\ClassLoader\XcacheClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
@@ -14,10 +14,14 @@ $loader->unregister();
 $apcLoader->register(true);
 */
 
+$xCacheLoader = new XcacheClassLoader('life-list', $loader);
+$loader->unregister();
+$xCacheLoader->register(true);
+
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel('prod', true);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
