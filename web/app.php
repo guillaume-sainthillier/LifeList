0589d80ea225cda6bf7cbd5b12234055ac28ecb4
Guillaume Sainthillier <guillaume@sainthillier.fr>
2014-10-01 10:06:44 +0200

    Mise à jour du fichier app.php

--- web/app.php
+++ web/app.php
@@ -5,28 +5,19 @@ use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
-/*
-$apcLoader = new ApcClassLoader('sf2', $loader);
+$XCacheLoader = new XcacheClassLoader(sha1(__FILE__), $loader);
 $loader->unregister();
-$apcLoader->register(true);
-*/
+$XCacheLoader->register(true);
 
-$xCacheLoader = new XcacheClassLoader('life-list', $loader);
-$loader->unregister();
-$xCacheLoader->register(true);
 
-require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+//require_once __DIR__.'/../app/AppKernel.php';
+require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$appKernel = new AppKernel('prod', false);
+$appKernel->loadClassCache();
+$kernel = new AppCache($appKernel);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
+Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
